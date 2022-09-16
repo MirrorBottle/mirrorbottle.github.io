cd399cda9788bb01080c3c635487d6dea433dd10
MirrorBottle <setiawanbayu66152@gmail.com>
2022-09-16 16:35:37 +0800

    Updates

--- gatsby-node.js
+++
@@ -1,6 +0,0 @@
-const express = require(`express`)
-
-// Enable development support for serving HTML from `./static` folder
-exports.onCreateDevServer = ({ app }) => {
-  app.use(express.static(`static`))
-}
