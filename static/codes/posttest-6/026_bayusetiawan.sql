c5eb816a2d14d30669740f21d9c95979bd712f8f
MirrorBottle <setiawanbayu66152@gmail.com>
2022-10-25 12:21:49 +0800

    feat: posttest-6

---
+++ static/codes/posttest-6/026_bayusetiawan.sql
@@ -0,0 +1,115 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 25, 2022 at 04:20 AM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 7.4.22
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `026_bayusetiawan`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `journals`
+--
+
+CREATE TABLE `journals` (
+  `id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `author` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `file` text NOT NULL,
+  `date` date NOT NULL,
+  `version` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `journals`
+--
+
+INSERT INTO `journals` (`id`, `title`, `author`, `description`, `file`, `date`, `version`) VALUES
+(2, 'Jurnal 3', 'Ser', 'Jurnal 2', 'https://classroom.google.com/u/0/c/NTI3MzE4NTY4MDk0/a/NTAyOTY3NzY5NTU3/details', '2022-10-05', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `journal_files`
+--
+
+CREATE TABLE `journal_files` (
+  `id` int(11) NOT NULL,
+  `journal_id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `file` varchar(200) NOT NULL,
+  `uploaded_at` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `journal_files`
+--
+
+INSERT INTO `journal_files` (`id`, `journal_id`, `name`, `file`, `uploaded_at`) VALUES
+(1, 2, 'File 3', '635763eb9a1e7.png', '2022-10-25 04:19:37');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `journals`
+--
+ALTER TABLE `journals`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `journal_files`
+--
+ALTER TABLE `journal_files`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `journal_id` (`journal_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `journals`
+--
+ALTER TABLE `journals`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `journal_files`
+--
+ALTER TABLE `journal_files`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `journal_files`
+--
+ALTER TABLE `journal_files`
+  ADD CONSTRAINT `journal_files_ibfk_1` FOREIGN KEY (`journal_id`) REFERENCES `journals` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ static/codes/posttest-6/journal-files/index.php
@@ -0,0 +1,128 @@
+
+<?php
+  include('../functions.php');
+
+  if(!isset($_GET['id'])) {
+    header("location:../index.php");
+  }
+
+  $journal = find("journals", $_GET['id']);
+  $files = query("SELECT * FROM journal_files WHERE journal_id={$journal['id']}");
+
+?>
+
+<!DOCTYPE html>
+<html lang="en" data-theme="light">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Bayu Setiawan - Posttest 6</title>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="../css/utility.css">
+  <link rel="stylesheet" href="../css/style.css">
+
+  <script src="https://kit.fontawesome.com/298ddad7ce.js" crossorigin="anonymous"></script>
+</head>
+<body>
+
+  <!-- HEADER -->
+  <header>
+    <div class="container row p-1">
+      <div class="col col-2 flex align-items-center justify-content-center">
+        <img id="logo" src="../images/Kertas-logos_black.png">
+      </div>
+      <div class="col col-8 text-center">
+        <nav class="flex align-items-center justify-content-center">
+          <ul class="flex align-items-center justify-content-around">
+            <li class="active"><a href="../index.php">Daftar Jurnal</a></li>
+          </ul>
+        </nav>
+      </div>
+      <div class="col col-2 flex align-items-center justify-content-center">
+        <button id="darkmode-toggle" class="btn btn-primary btn-block">
+          <i class="fa-solid fa-sun"></i>
+        </button>
+      </div>
+    </div>
+  </header>
+
+  <main id="home-page">
+    
+  <section id="static" class="container row pt-4 mt-4 pb-4 mb-4">
+      <div class="row w-100">
+        <div class="col col-8">
+          <h1>Daftar File - <?= $journal['title'] ?></h1>
+        </div>
+        <div class="col col-4 flex align-items-center justify-content-end">
+          <a href="./create.php?journal_id=<?= $journal['id']  ?>" class="btn btn-primary">
+            <i class="fa-solid fa-plus mr-2"></i>
+            Tambah File
+          </a>
+        </div>
+        <div class="col col-12 mt-3">
+          <table class="w-100" border="1">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Nama File</th>
+                <th>Tanggal Upload</th>
+                <th>Aksi</th>
+              </tr>
+            </thead>
+            <tbody>
+              <?php $i = 1; ?>
+              <?php foreach ($files as $file) : ?>
+                <tr>
+                  <th scope="row"><?= $i ?></th>
+                  <td><?= $file['name'] ?></td>
+                  <td><?= $file['uploaded_at'] ?></td>
+                  <td class="text-center">
+                    <a href="../storage/<?= $file['file'] ?>" download class="btn btn-primary mr-1">
+                      <i class="fa-solid fa-download"></i>
+                    </a>
+                    <a href="./edit.php?journal_id=<?= $journal['id']  ?>&id=<?= $file['id'] ?>" class="btn btn-primary mr-1">
+                      <i class="fa-solid fa-pencil"></i>
+                    </a>
+                    <a data-id="<?= $file['id'] ?>" class="btn btn-primary delete-btn">
+                      <i class="fa-solid fa-trash"></i>
+                    </a>
+                  </td>
+                </tr>
+                <?php $i++; ?>
+              <?php endforeach; ?>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </section>
+  </main>
+
+
+  <footer class="bg-accent">
+    <p>Kertas. All right reserved</p>
+  </footer>
+  <script src="https://code.jquery.com/jquery-3.5.1.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/sweetalert2@10/dist/sweetalert2.all.min.js"></script>
+  <script>
+    $(document).ready(function() {
+      $(document).on('click', '.delete-btn', function() {
+        Swal.fire({
+          title: 'Apa anda yakin?',
+          text: "Datanya bakal hilang, loh!",
+          icon: 'warning',
+          showCancelButton: true,
+          confirmButtonColor: '#3085d6',
+          cancelButtonColor: '#d33',
+          confirmButtonText: 'Ya!',
+          cancelButtonText: 'Gak Jadi!'
+        }).then((result) => {
+          window.location.href = `./delete.php?id=${$(this).data('id')}`
+        })
+      })
+    });
+  </script>
+</body>
+</html>
